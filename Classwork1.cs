13086ca823fdd98d84a9f24bd6cabf47cff49484
ValkyrieQ8 <zahraa_amiri@hotmail.com>
2022-06-26 20:58:51 +0300

    class1

---
+++ Classwork1.cs
@@ -0,0 +1,17 @@
+﻿//Hero
+string HeroName = "Assassin";
+int HeroHeight = 167;
+string HeroSuperPower = "Hidden Blade";
+int HeroAge = 38;
+
+//Villain
+string VillainName = "Templar";
+int VillainHeight = 170;
+string VillainSuperPower = "Apple of eden";
+int VillainAge = 45;
+
+//Age Diff
+int AgeDiff = VillainAge - HeroAge;
+
+//Story
+Console.WriteLine("The hero of this story is " + HeroName +  "his height " + HeroHeight + " and lived to the age " + HeroAge + " his greatest strength lays in his arm, the " + HeroSuperPower + " compared to the villain " + VillainName + " that surprasses him in height " + VillainHeight + " and carrying the " + VillainSuperPower + ", the hero's sucsses is low but never zero. their age difference is " + AgeDiff);
